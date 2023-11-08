71b717523ae585f87d45d601d1d3084115090364
Cezary Pukownik <cezary.pukownik@gmail.com>
2023-11-08 15:38:13 +0100

    Script will store data in S3

--- extract_justjoinit.py
+++ extract_justjoinit.py
@@ -1,25 +1,49 @@
 import os
+import sys
 import time
 import json
+import logging
 from datetime import datetime
 
+import boto3
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
 # SETTINGS
-DATA_ROOT = "data"
+DATA_ROOT = "/tmp/skilzzz/justjoinit"
+S3_ROOT = "justjoinit"
+S3_BUCKET = "stxnext-sandbox-cp"
 # ########
 
 def scrape_justjoinit_offer_list(url: str, scroll_interval: float = 0.2, init_sleep: int = 3, scroll_by: int = 800):
     """This funtion scrape page by loading url and slowly scrolling down. At each scroll html in yielded."""
+
+    logger.info(f"Started Scraping JustJoinIT from url: {url}")
+    logger.info(f"Scraping parameters: scroll_interval: {scroll_interval}, scroll_by: {scroll_by}, init_sleep: {init_sleep}")
+    
+    logger.info("Launching Headless Browser...")
     options = Options()
     options.add_argument('--headless=new')
     driver = webdriver.Chrome(options=options)
+
+    logger.info(f"Openning url {url}...")
     driver.get(url)
 
+    logger.info(f"Waiting for {init_sleep} seconds...")
     time.sleep(init_sleep)
+
+    logger.info("Started scrolling")
     while True:
         driver.execute_script(f"""window.scrollBy(0, {scroll_by})""")
         time.sleep(scroll_interval) # seconds
@@ -40,10 +64,18 @@ def scrape_justjoinit_offer_list(url: str, scroll_interval: float = 0.2, init_sl
         if scroll_top+client_height >= scroll_height:
             break
 
-        # Return rendered HTML
-        yield driver.page_source
+        logger.info(f"Current position: {scroll_top+client_height}, Page size: {scroll_height}")
 
+        # Return rendered HTML
+        html = driver.page_source
+
+        logger.info(f"Yielded page html ({sys.getsizeof(html)})")
+        yield html
+
+    logger.info("Got to the end of page.")
     # Close the ChromeDriver instance
+
+    logger.info("Quit headless browser.")
     driver.quit()
 
 
@@ -59,10 +91,12 @@ def parse_justjoinit_html(html):
         orders = page['data']
         all_orders.extend(orders)
 
+
+    logger.info(f"Parsed {len(all_orders)} orders.")
     return all_orders
 
 
-def generate_session_folder(root_folder: str) -> str:
+def generate_session_folder() -> str:
     """
     Generate a filepath with timestamp in the format '/year=YYYY/month=MM/day=DD/filename_YYYYMMDDHHMMSS.jsonl'.
 
@@ -78,49 +112,65 @@ def generate_session_folder(root_folder: str) -> str:
     month = current_time.strftime("%m")
     day = current_time.strftime("%d")
 
-    session_folder = f"{root_folder}/year={year}/month={month}/day={day}/{timestamp}"
+    session_folder = f"year={year}/month={month}/day={day}/{timestamp}"
     return session_folder
 
 def extract_justjoinit():
+    # S3 Service
+    s3 = boto3.client("s3")
+
     # Extract to html files.
     url = "https://justjoin.it/all-locations/data"
 
     # JSON
-    json_folder = generate_session_folder(f"{DATA_ROOT}/json")[:-14]
-    html_folder = generate_session_folder(f"{DATA_ROOT}/html")
+    session_path = generate_session_folder()
+    local_json_folder = f"{DATA_ROOT}/json/" + session_path[:-15] # without timestamp folder
+    local_html_folder = f"{DATA_ROOT}/html/" + session_path
+
     session_started_at = datetime.now()
     session_timestamp = session_started_at.strftime("%Y%m%d%H%M%S")
 
-    os.makedirs(json_folder, exist_ok=True)
-    os.makedirs(html_folder, exist_ok=True)
+    os.makedirs(local_json_folder, exist_ok=True)
+    os.makedirs(local_html_folder, exist_ok=True)
 
-    with open(f'{json_folder}/justjoinit-{session_timestamp}.jsonl', 'w') as f_json:
-
+    json_name = f"justjoinit-{session_timestamp}.jsonl" 
+    local_json_path = f'{local_json_folder}/{json_name}'
+    with open(local_json_path, 'w') as f_json:
         # Start Extracting HTMLs
-        for i, html in tqdm(enumerate(scrape_justjoinit_offer_list(url))):
+        for i, html in enumerate(scrape_justjoinit_offer_list(url)):
 
             # Save HTML file.
             html_name = f"justjoinit-{session_timestamp}-{i}.html"
-            html_path = f"{html_folder}/{html_name}"
-            with open(html_path, "w") as f_html:
+            local_html_path = f"{local_html_folder}/{html_name}"
+            with open(local_html_path, "w") as f_html:
                 f_html.write(html)
+
+            s3_path = f"{S3_ROOT}/html/{session_path}/{html_name}"
+            s3.upload_file(local_html_path, S3_BUCKET, s3_path)
+            logger.info(f"Saved HTML file to S3: {s3_path}")
 
             # Parse HTML and write to JSON
             try:
                 orders = parse_justjoinit_html(html)
             except:
-                print(f"{datetime.now()} [WARNING] JSON Data not found in html file {html_path}.")
+                logger.warning(f"JSON Data not found in html file {s3_path}.")
 
             metadata = {
                 "_scraped_at": session_started_at,
                 "_scraped_from": url,
                 "_loaded_at": datetime.now(),
-                "_source_html": html_path,
+                "_source_html": s3_path,
             }
+
             jsonl_str = '\n'.join([json.dumps({**order, **metadata}, default=str) for order in orders]) + '\n'
             f_json.write(jsonl_str)
+
+    s3_path = f"{S3_ROOT}/json/{session_path[:-15]}/{json_name}"
+    s3.upload_file(local_json_path, S3_BUCKET, s3_path)
+    logger.info(f"Saved JSON file to S3: {s3_path}")
    
 if __name__=="__main__": 
     extract_justjoinit()
+    logger.info(f"Success!")
     
 
